a0e940e091e84a1743fe3d6ad32228a30094078c
danielnagy600 <31795389+danielnagy600@users.noreply.github.com>
2020-07-28 16:46:21 +0200

    Add files via upload

---
+++ Audio/MusicManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicManager : MonoBehaviour
+{
+    private AudioSource Audio;
+    public AudioClip GameHorrorMusic;
+
+    public  void Awake()
+    {
+        Audio = GetComponent<AudioSource>();
+    }
+
+    public void Start()
+    {
+        if (SceneManager.GetSceneByName("Game").IsValid())
+        {
+            Audio.GetComponent<MusicManager>().ChangeAudio(GameHorrorMusic);
+        } 
+    }
+
+
+
+    public void ChangeAudio(AudioClip NewAudio)
+    {
+        Audio.Stop();
+        Audio.clip = NewAudio;
+        Audio.Play();
+    }
+
+}
---
+++ Audio/ZombieAudio.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieAudio : MonoBehaviour
+{
+    private AudioSource ZombieVoice;
+
+    public void Awake()
+    {
+        ZombieVoice = GetComponent<AudioSource>();
+   
+    }
+
+    public void Death()
+    {
+        //Életereje lecsökken
+        ZombieVoice.Stop();
+    }
+}
